e72f676fb2326a06a521f807e70f3046e074e550
RowanKinross <rowankinross@yahoo.co.uk>
2024-01-17 21:58:30 +0000

    limit_search_history_to_5_items

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,34 @@
 //const APIKey = `` //<--insert own API key here
 const searchHistory = $(`#history`)
 const searchArr = [];
+const clearAllButton = $(`<button>`).text(`clear past worms`).addClass(`btn btn-warning clearAll`)
 
 //__________________
 //Rowan :
 
 //Get any locally stored search history
+function makeButtonsFromSearchArray() {
+  searchHistory.empty()
 if (Object.entries(localStorage) != ``){
   const storedArr = localStorage.getItem(`searchArr`).split(`,`)
-  searchHistory.empty()
+  // prepend search history buttons
   if (storedArr != null){
+    // append a clear all button
+    searchHistory.append(clearAllButton)
     for (let i=0; i<storedArr.length; i++){
-    song = storedArr[i]
-    prependSongButton()
-    searchArr.push(song)
+      song = storedArr[i]
+      prependSongButton()
     }
   // load music info function()
 }}
+}
+makeButtonsFromSearchArray()
 
 //prepend searched for songs as buttons in a function:
 function prependSongButton(){
   if (song != ``){
     const songButton = $(`<button>`).text(song).addClass(`btn btn-outline-primary m-1`).attr(`song-name`, song)
-    const clearButton = $(`<button>`).text(`X`).addClass(`btn btn-warning clearButton`)
-    const songDiv = $(`<div>`).append(songButton, clearButton)
+    const songDiv = $(`<div>`).append(songButton)
     searchHistory.prepend(songDiv);
 }}
 
@@ -34,20 +39,23 @@ $(`#search-form`).on(`submit`, function(e){
     // load the music info for that song
         //call load music info function
 
-    //if the searchArr includes the song already, remove it and push it again(to the start)
-    if (searchArr.includes(song)){
-      const index = searchArr.indexOf(song)
-      searchArr.splice(index, 1);}
-      else{
-      // add the song as a button in the search history
-      prependSongButton()
+    //if the searchArr is longer than 5, remove the 5th and push the most recent
+    if (searchArr.length>=5){
+      searchArr.splice(0, 1);}
+    // push song to searchArr
+    searchArr.push(song)
+    // set the array in local storage
+    localStorage.setItem(`searchArr`, searchArr)
+    makeButtonsFromSearchArray()
       }
-      // push song to searchArr
-      searchArr.push(song)
-      // set the array in local storage
-      localStorage.setItem(`searchArr`, searchArr)
-  }
 )
+
+//CLICK event when a user clicks to clear search
+// $(`.clearAll`).on(`click`, function(e){
+//   e.preventDefault()
+//   localStorage.clear()
+//   searchHistory.empty()
+// })
 
 //CLICK event when a user clicks on search history
 searchHistory.on(`click`, function(e){
@@ -56,15 +64,8 @@ searchHistory.on(`click`, function(e){
       //call load music info function 
 })
 
-//CLICK event when a user clicks on search history `X`
-$(`.clearButton`).on(`click`, function(e){
-  // e.preventDefault()
-  // var targetX = e.target
-  // var parentToX = targetX.parentElement
-  // parentToX.remove();
-})
 
-//clear search history button
+
 //CLICK event when a user clicks on on clear search history
 
 
