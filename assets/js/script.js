5f79b0e298855efa2dcbca250be16a43a44d61d5
RowanKinross <rowankinross@yahoo.co.uk>
2024-01-18 16:53:14 +0000

    clear_search_history_works_button_added_to_html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,23 @@
 //const APIKey = `` //<--insert own API key here
 const searchHistory = $(`#history`)
 const searchArr = [];
-const clearAllButton = $(`<button>`).text(`clear past worms`).addClass(`btn btn-warning clearAll`)
+const clearAllButton = $(`.clearAll`);
 
 //__________________
 //Rowan :
 
 //Get any locally stored search history
-function makeButtonsFromSearchArray() {
   searchHistory.empty()
 if (Object.entries(localStorage) != ``){
   const storedArr = localStorage.getItem(`searchArr`).split(`,`)
   // prepend search history buttons
   if (storedArr != null){
-    // append a clear all button
-    searchHistory.append(clearAllButton)
     for (let i=0; i<storedArr.length; i++){
       song = storedArr[i]
       prependSongButton()
     }
   // load music info function()
 }}
-}
-makeButtonsFromSearchArray()
 
 //prepend searched for songs as buttons in a function:
 function prependSongButton(){
@@ -38,36 +33,26 @@ $(`#search-form`).on(`submit`, function(e){
   song = $(`#search-input`).val().trim().toUpperCase()
     // load the music info for that song
         //call load music info function
-
     //if the searchArr is longer than 5, remove the 5th and push the most recent
     if (searchArr.length>=5){
       searchArr.splice(0, 1);}
     // push song to searchArr
     searchArr.push(song)
+    // prepend search button
+    prependSongButton();
     // set the array in local storage
     localStorage.setItem(`searchArr`, searchArr)
-    makeButtonsFromSearchArray()
+    //clear form search bar
+    $(`#search-input`).val(``)
       }
 )
 
 //CLICK event when a user clicks to clear search
-// $(`.clearAll`).on(`click`, function(e){
-//   e.preventDefault()
-//   localStorage.clear()
-//   searchHistory.empty()
-// })
-
-//CLICK event when a user clicks on search history
-searchHistory.on(`click`, function(e){
+clearAllButton.on(`click`, function(e){
   e.preventDefault()
-  song = e.target.innerHTML
-      //call load music info function 
+  localStorage.clear()
+  searchHistory.empty()
 })
-
-
-
-//CLICK event when a user clicks on on clear search history
-
 
 
 
